fd1f0075ff07c2825041ad4f43b9f0cca378234e
Denis <ulman33312@gmail.com>
2023-08-10 01:24:46 +0300

    feat: added responsive design and burger menu

--- library/src/js/scripts.js
+++ library/src/js/scripts.js
@@ -10,3 +10,33 @@ libraryCardInput.forEach((input) => {
     e.target.value = e.target.value.replace(regex, '');
   });
 });
+
+//burger
+const menu = document.querySelector('.menu__body');
+const menuBtn = document.querySelector('.menu__icon');
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    body.classList.toggle('lock')
+  })
+
+  menu.addEventListener('click', e => {
+    if (e.target.classList.contains('menu__body')) {
+      menu.classList.remove('active')
+      menuBtn.classList.remove('active')
+      body.classList.remove('lock')
+    }
+  })
+
+  menu.querySelectorAll('.menu__link').forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('active')
+      menuBtn.classList.remove('active')
+      body.classList.remove('lock')
+    })
+  })
+}
